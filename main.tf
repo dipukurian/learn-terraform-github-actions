e546b803351325c48a5e45fcca2ace26edf11eab
dipu.mathew <cloud_user@3b343d66681c.mylabserver.com>
2024-08-27 16:21:35 +0000

    updating organization & workspaces

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Everyones!" > index.html
+              echo "Hello, Matrix!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
