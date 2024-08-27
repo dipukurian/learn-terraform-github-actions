a7c7e3a1f441f9cacb7d4f541a43385bbef1653b
dipu.mathew <cloud_user@3b343d66681c.mylabserver.com>
2024-08-27 16:35:08 +0000

    updating organization & workspaces

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
+  ami                    = "ami-064519b8c76274859"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
